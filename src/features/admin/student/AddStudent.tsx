2cabb490b147288dbccd006d2e8e2226ef64dd17
Mohamed atef <89284188+hngara@users.noreply.github.com>
2024-06-29 02:44:31 +0300

    add student refactor

--- src/features/admin/student/AddStudent.tsx
+++ src/features/admin/student/AddStudent.tsx
@@ -8,7 +8,13 @@ import { useSnackbar } from "notistack";
 import { createStudentDto } from "./type";
 import Header from "@/components/Header";
 import { InputField } from "@/components/inputs/InputField";
-import { codeValidation, genderValidation, joiningYearValidation, nameValidation, passwordValidation } from "@/constants/validations";
+import {
+  codeValidation,
+  genderValidation,
+  joiningYearValidation,
+  nameValidation,
+  passwordValidation,
+} from "@/constants/validations";
 import { PasswordField } from "@/components/inputs/PasswordField";
 import { SelectField } from "@/components/inputs/SelectField";
 import CustomLoadingButton from "@/components/buttons/CustomLoadingButton";
@@ -20,8 +26,8 @@ function AddStudent() {
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
   const [addStudent, { isLoading }] = useAddStudentMutation();
 
-  const departmentQuery = useGetAllDepartmentQuery({ pageNo: 0, pageSize: 20 });
-  const departmentsData = departmentQuery.data?.content || [];
+  const departmentQuery = useGetAllDepartmentQuery();
+  const departmentsData = departmentQuery.data || [];
   console.log("departmentsData", departmentsData);
   const extractedData = departmentsData.map((item) => ({
     value: item.code,
@@ -62,109 +68,93 @@ function AddStudent() {
         sx={{
           display: "flex",
           flexDirection: "column",
-        
-           width: isSmallScreen ? "100%" : "75%",
-           mx:"auto"
+
+          width: isSmallScreen ? "100%" : "75%",
+          mx: "auto",
         }}
         onSubmit={handleSubmit(onSubmit)}
         noValidate
         autoComplete="off"
       >
-        <Stack
-          direction={isSmallScreen ? "column" : "row"}
-          sx={{ gap: isSmallScreen ? 0 : 2 }}
-        >
+        <Stack direction={isSmallScreen ? "column" : "row"} sx={{ gap: isSmallScreen ? 0 : 2 }}>
           <InputField
             label="First Name"
-            inputProps={register("firstName", {...nameValidation,deps:["firstName"]})}
+            inputProps={register("firstName", { ...nameValidation, deps: ["firstName"] })}
             error={Boolean(errors.firstName)}
             helperText={Boolean(errors.firstName) === true ? "Required" : " "}
             variant={"outlined"}
           />
           <InputField
             label="Last Name"
-            inputProps={register("lastname", {...nameValidation,deps:["lastname"]})}
+            inputProps={register("lastname", { ...nameValidation, deps: ["lastname"] })}
             error={Boolean(errors.lastname)}
             helperText={Boolean(errors.lastname) === true ? "Required" : " "}
             variant={"outlined"}
           />
         </Stack>
-        <Stack
-          direction={isSmallScreen ? "column" : "row"}
-          sx={{ gap: isSmallScreen ? 0 : 2 }}
-        >
+        <Stack direction={isSmallScreen ? "column" : "row"} sx={{ gap: isSmallScreen ? 0 : 2 }}>
           <InputField
             label="Father Name"
-            inputProps={register("fatherName", {...nameValidation,deps:["fatherName"]})}
+            inputProps={register("fatherName", { ...nameValidation, deps: ["fatherName"] })}
             error={Boolean(errors.fatherName)}
             helperText={Boolean(errors.fatherName) === true ? "Required" : " "}
             variant={"outlined"}
           />
           <InputField
             label="Grandfather Name"
-            inputProps={register("grandfatherName", {...nameValidation,deps:["grandfatherName"]})}
+            inputProps={register("grandfatherName", {
+              ...nameValidation,
+              deps: ["grandfatherName"],
+            })}
             error={Boolean(errors.grandfatherName)}
-            helperText={
-              Boolean(errors.grandfatherName) === true ? "Required" : " "
-            }
+            helperText={Boolean(errors.grandfatherName) === true ? "Required" : " "}
             variant={"outlined"}
           />
         </Stack>
-        <Stack
-          direction={isSmallScreen ? "column" : "row"}
-          sx={{ gap: isSmallScreen ? 0 : 2 }}
-        >
+        <Stack direction={isSmallScreen ? "column" : "row"} sx={{ gap: isSmallScreen ? 0 : 2 }}>
           <InputField
             label="Joining Year"
-            inputProps={register("joining_year", {...joiningYearValidation,deps:["joining_year"]})}
+            inputProps={register("joining_year", {
+              ...joiningYearValidation,
+              deps: ["joining_year"],
+            })}
             error={Boolean(errors.joining_year)}
-            helperText={
-              Boolean(errors.joining_year) === true ? "Required" : " "
-            }
+            helperText={Boolean(errors.joining_year) === true ? "Required" : " "}
             variant={"outlined"}
           />
         </Stack>
-        <Stack
-          direction={isSmallScreen ? "column" : "row"}
-          sx={{ gap: isSmallScreen ? 0 : 2 }}
-        >
+        <Stack direction={isSmallScreen ? "column" : "row"} sx={{ gap: isSmallScreen ? 0 : 2 }}>
           <InputField
             label="Address"
-            inputProps={register("address", {...nameValidation,deps:["address"]})}
+            inputProps={register("address", { ...nameValidation, deps: ["address"] })}
             error={Boolean(errors.address)}
             helperText={Boolean(errors.address) === true ? "Required" : " "}
             variant={"outlined"}
           />
         </Stack>
-        <Stack
-          direction={isSmallScreen ? "column" : "row"}
-          sx={{ gap: isSmallScreen ? 0 : 2 }}
-        >
+        <Stack direction={isSmallScreen ? "column" : "row"} sx={{ gap: isSmallScreen ? 0 : 2 }}>
           <InputField
             label="Code"
-            inputProps={register("code", {...codeValidation,deps:["code"]})}
+            inputProps={register("code", { ...codeValidation, deps: ["code"] })}
             error={Boolean(errors.code)}
             helperText={Boolean(errors.code) === true ? "Required" : " "}
             variant={"outlined"}
           />
         </Stack>
 
-        <Stack
-          direction={isSmallScreen ? "column" : "row"}
-          sx={{ gap: isSmallScreen ? 0 : 2 }}
-        >
+        <Stack direction={isSmallScreen ? "column" : "row"} sx={{ gap: isSmallScreen ? 0 : 2 }}>
           <PasswordField
-            inputProps={register("password", {...passwordValidation,deps:["password"]})}
+            inputProps={register("password", { ...passwordValidation, deps: ["password"] })}
             error={Boolean(errors.password)}
           />
         </Stack>
         <Stack
           direction={isSmallScreen ? "column" : "row"}
-          sx={{ gap: isSmallScreen ? 0 : 2 , mt: 2 }}
+          sx={{ gap: isSmallScreen ? 0 : 2, mt: 2 }}
         >
           <SelectField
             label="Gender"
-            inputProps={register("gender", {...genderValidation,deps:["gender"]})}
+            inputProps={register("gender", { ...genderValidation, deps: ["gender"] })}
             error={Boolean(errors.gender)}
             helperText={Boolean(errors.gender) === true ? "Required" : " "}
             options={StudentGender}
@@ -173,9 +163,7 @@ function AddStudent() {
             label="Department"
             inputProps={register("department_code", { required: true })}
             error={Boolean(errors.department_code)}
-            helperText={
-              Boolean(errors.department_code) === true ? "Required" : " "
-            }
+            helperText={Boolean(errors.department_code) === true ? "Required" : " "}
             options={extractedData}
           />
         </Stack>
